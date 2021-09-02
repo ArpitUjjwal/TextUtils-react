1d8d4e04f1988705bce93cd186e0b4538809b466
Arpit <arpitujjwal1@gmail.com>
2021-09-02 20:32:21 +0530

    Dark Mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -50,10 +50,10 @@ export default function TextForm(props) {
     const [text, setText] = useState('');    
     return (
         <>
-        <div className="container">
+        <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea placeholder="Your text goes here" className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea placeholder="Your text goes here" className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#BEBEBE':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to Uppercase</button>
             <button className="btn btn-secondary mx-1" onClick={handleLoClick} >Convert to Lowercase</button>
@@ -61,16 +61,15 @@ export default function TextForm(props) {
             <button className="btn btn-info mx-1" onClick={handleTitleCase} >Convert to Title Case</button>
             <button className="btn btn-danger mx-1" onClick={handleClearClick} >Clear Text</button>
             <button className="btn btn-success mx-1" onClick={handleCopyClick} >Copy Text</button>
-            
+            <hr/>
         </div>
-        <hr/>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} words , {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes to read</p>
             <hr/>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Text Field is empty"}</p>
         </div>
         </>
     )
