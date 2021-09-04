93e790c3f4dab2736ff7b2cb5f2854ac9593132a
Arpit <arpitujjwal1@gmail.com>
2021-09-04 15:27:45 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
 import Alert from './components/Alert'
-import About from './components/About';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+// import About from './components/About';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
 
 
 function App() {
@@ -44,20 +43,20 @@ function App() {
 
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert} />
     <div className="container my-3">
-    <Switch>
+    {/* <Switch>
           <Route exact path="/about">
             <About />
           </Route>
-          <Route exact path="/">
+          <Route exact path="/"> */}
             <TextForm showAlert={showAlert} showheading="Enter the text to analyze below" mode={mode}/>
-          </Route>
-    </Switch>
+          {/* </Route> */}
+    {/* </Switch> */}
     </div>
-    </Router>
+    {/* </Router> */}
     </>
   );
 }
